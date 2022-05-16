920e2c592dbc97acaf3224233cb7b3f6b06dec79
Anselm Joseph <anselmjs@icloud.com>
2022-05-16 22:41:45 +0530

    troubleshooting

---
+++ src/Troubles.tsx
@@ -0,0 +1,14 @@
+import './Troubles.css'
+import React from 'react'
+
+export default function Troubles() {
+  return (
+    <div className="Trouble-main">
+      <div className="Trouble-content">
+        <h1>Troubleshooting</h1>
+        <p>1. Check if your Windows PC, iPhone and Apple Watch are on the same WiFi/Local network. If you have 2 bands of WiFi 2.5Ghz and 5Ghz please try both.</p>
+        <p>2. If your iOS App does not connect to your Apple Watch App, please restart both apps.</p>
+      </div>
+    </div>
+  )
+}
